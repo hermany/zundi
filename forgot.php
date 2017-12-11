f838ef45ad33f77200dfa052361a7b521f1213ee
Hermany <hterrazas@gmail.com>
2017-12-11 09:26:03 -0400

    2.5.24
    - Cambio a concepto file.local para trabajar desde el nucleo los cambios de dashboard, login, forgot y logout. cambio a carpeta dasboard y login en modulos nucleo, y en el sitio a :
    * dashboard.local.php
    * login.local.php
    * logout.local.php
    * forgot.local.php

--- forgot.php
+++ forgot.php
@@ -1,31 +1,4 @@
 <?php
 require_once("config.php");
-require_once(_RUTA_NUCLEO."clases/class-constructor.php");
-$fmt = new CONSTRUCTOR();
-
-echo $fmt->header->header_html();
-//$fmt->header->title_page("Logeo - Next Sistemas Integrados");
+require_once(_RUTA_NUCLEO."modulos/login/forgot.local.php");
 ?>
-</head>
-<body class='body-login container-fluid'>
-
-  <link rel="stylesheet" href="<? echo _RUTA_WEB_NUCLEO; ?>css/estilos.adm.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="<? echo _RUTA_WEB_NUCLEO; ?>css/theme.adm.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="<? echo _RUTA_WEB_NUCLEO; ?>css/icon-font.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="<? echo _RUTA_WEB_NUCLEO; ?>css/font-sourse-sans.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="<? echo _RUTA_WEB_NUCLEO;?>css/animate.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="<? echo _RUTA_WEB_NUCLEO; ?>css/login.adm.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="<? echo _RUTA_WEB_NUCLEO; ?>css/login-theme.adm.css" rel="stylesheet" type="text/css">
-  <?PHP if (_THEME_DEFAULT){ ?>
-  <link rel="stylesheet" href="<? echo _THEME_DEFAULT;?>" rel="stylesheet" type="text/css">
-  <?php } ?>
-  <?php
-    echo $fmt->header->js_jquery();
-    require_once(_RUTA_NUCLEO."modulos/login/forgot.form.php");
-  ?>
-  <div class="login-footer">
-    2016-2017 <strong>®</strong> Wappcom &nbsp; | &nbsp;  power <i class="icn-cc"></i> <?php echo _VZ; ?>
-  </div>
-  <script type="text/javascript" language="javascript" src="<? echo _RUTA_WEB_NUCLEO; ?>js/core.js"></script>
-</body>
-</html>
--- logout.php
+++ logout.php
@@ -1,16 +1,4 @@
 <?php
 require_once("config.php");
-require_once(_RUTA_NUCLEO."clases/class-constructor.php");
-
-		$fmt = new CONSTRUCTOR();
-		$fmt->sesion->iniciar_sesion();
-		$fmt->sesion->cerrar_sesion();
-
-		// $fmt->get->validar_get($_GET["cat"]);
-		// $fmt->get->validar_get($_GET["pla"]);
-		// $cat =$_GET["cat"];
-		// $pla =$_GET["pla"];
-		// $link="index.php?&cat=".$cat."&pla=".$pla;
-		$link=_RUTA_WEB."login";
-		header("Location:".$link);
+require_once(_RUTA_NUCLEO."modulos/login/logout.local.php");
 ?>
